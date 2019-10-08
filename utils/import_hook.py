f6047322a6090af75c6676dab831599d6c704e38
Harshit Singh Lodha <harshitandro@gmail.com>
2019-10-08 23:46:45 +0530

    Fixed issue with nested class hooking & code cleanup

--- utils/import_hook.py
+++ utils/import_hook.py
@@ -1,11 +1,8 @@
 from __future__ import print_function
 
 import sys
-import traceback
 
-from constants.hooks import USER_CALLABLES_TO_HOOK
-from utils.callbacks import start_callback, end_callback, error_callback
-from utils.instrumentation import instrument
+from utils.instrumentation import apply_hooks
 
 if sys.version_info[0] == 2:
     import __builtin__ as builtin
@@ -23,56 +20,13 @@ old_reload = imp.reload
 
 def custom_import(*args, **kwargs):
     module = old_imp(*args, **kwargs)
-    # Invoking hooking logic at time of module loading
-    try:
-        if module.__name__ in list(USER_CALLABLES_TO_HOOK.keys()):
-            for func_string in USER_CALLABLES_TO_HOOK[module.__name__]:
-                cls = None
-                func_str = func_string
-                if func_str.find(".") != -1:
-                    func_str_parts = func_str.split(".")
-                    for part_i in range(len(func_str_parts)-1):
-                        class_str = func_str_parts[part_i]
-                        func_str = func_str_parts[part_i+1]
-                        if cls is None:
-                            cls = getattr(module, class_str)
-                        else:
-                            cls = getattr(cls, class_str)
-                if cls is not None:
-                    func = getattr(cls, func_str)
-                    setattr(cls, func_str,
-                            instrument(func, start_callback, end_callback, error_callback, isMethod=True))
-                else:
-                    func = getattr(module, func_str)
-                    setattr(module, func_str,
-                            instrument(func, start_callback, end_callback, error_callback))
-    except:
-        print("Error caught in import hook : {}".format(sys.exc_info()))
-        traceback.print_exc()
+    module = apply_hooks(module, is_system_hook=False)
     return module
 
 
 def custom_reload(*args, **kwargs):
     module = old_reload(*args, **kwargs)
-    # Invoking hooking logic at time of module loading
-    try:
-        if module.__name__ in list(USER_CALLABLES_TO_HOOK.keys()):
-            for func_string in USER_CALLABLES_TO_HOOK[module.__name__]:
-                cls = None
-                func_str = func_string
-                if func_str.find(".") != -1:
-                    class_str, func_str = func_str.split(".")
-                    cls = getattr(module, class_str)
-                if cls is not None:
-                    func = getattr(cls, func_str)
-                    setattr(cls, func_str,
-                            instrument(func, start_callback, end_callback, error_callback, isMethod=True))
-                else:
-                    func = getattr(module, func_str)
-                    setattr(module, func_str,
-                            instrument(func, start_callback, end_callback, error_callback))
-    except:
-        print("Error caught : {}".format(sys.exc_info()))
+    module = apply_hooks(module, is_system_hook=False)
     return module
 
 
@@ -80,19 +34,3 @@ def enable_module_hook_on_load():
     """This will enable the instrumentation of user modules upon load."""
     builtin.__import__ = custom_import
     imp.reload = custom_reload
-
-
-# def disable_module_hook_on_load():
-#     """Counter function of enable_module_hook_on_load"""
-#     builtin.__import__ = old_imp
-#     imp.reload = old_reload
-
-
-def reload_modules():
-    for mod in sys.modules.values():
-        if mod is not None:
-            print("Reloading module : {}".format(mod), file=sys.stderr)
-            try:
-                imp.reload(mod)
-            except:
-                print("Failed to reload module : {} : {}".format(mod, *sys.exc_info()), file=sys.stderr)
--- utils/instrumentation.py
+++ utils/instrumentation.py
@@ -1,30 +1,27 @@
 import sys
+import traceback
 from importlib import import_module
 
-from constants.hooks import BUILTIN_CALLABLES_TO_HOOK
+from constants.hooks import BUILTIN_CALLABLES_TO_HOOK, USER_CALLABLES_TO_HOOK
 from utils.callbacks import start_callback, end_callback, error_callback
 
 
-def instrument(func, startCallback, endCallback, errorCallback, isMethod=False):
+def instrument(func, source_string, startCallback, endCallback, errorCallback, isMethod=False):
     """This function is used to instrument the given method/function 'func'."""
     old_func = func
 
     def new_func(*args, **kwargs):
-        if isMethod:
-            source = func.__module__ + "." + args[0].__class__.__name__ + "." + func.__name__
-        else:
-            source = func.__module__ + "." + func.__name__
-        startCallback(source, *args, **kwargs)
+        startCallback(source_string, *args, **kwargs)
         try:
             ret = old_func(*args, **kwargs)
         except:
-            errorCallback(source, *sys.exc_info())
+            errorCallback(source_string, *sys.exc_info())
             # raise so that the user code can handle the thrown error
             raise
-        endCallback(source, ret)
+        endCallback(source_string, ret)
         return ret
 
-    print("Hooked method : {} of {}".format(func.__name__, func.__module__))
+    print("Hooked API : {}".format(source_string))
     return new_func
 
 
@@ -33,18 +30,47 @@ def instrument_system_api():
     for module_str in BUILTIN_CALLABLES_TO_HOOK.keys():
         try:
             module = import_module(module_str)
-            for func_string in BUILTIN_CALLABLES_TO_HOOK[module_str]:
+            apply_hooks(module, is_system_hook=True)
+        except:
+            # print("Error caught in import hook : {}".format(sys.exc_info()))
+            # traceback.print_exc()
+            pass
+
+
+def apply_hooks(module, is_system_hook=False):
+    HOOK_MAP= {}
+    if is_system_hook:
+        HOOK_MAP = BUILTIN_CALLABLES_TO_HOOK
+    else:
+        HOOK_MAP = USER_CALLABLES_TO_HOOK
+
+    # Invoking hooking logic at time of module loading
+    try:
+        if module.__name__ in list(HOOK_MAP.keys()):
+            for func_string in HOOK_MAP[module.__name__]:
+                source_string = module.__name__ + "." + func_string
                 cls = None
                 func_str = func_string
                 if func_str.find(".") != -1:
-                    class_str, func_str = func_str.split(".")
-                    cls = getattr(module, class_str)
+                    func_str_parts = func_str.split(".")
+                    for part_i in range(len(func_str_parts) - 1):
+                        class_str = func_str_parts[part_i]
+                        func_str = func_str_parts[part_i + 1]
+                        if cls is None:
+                            cls = getattr(module, class_str)
+                        else:
+                            cls = getattr(cls, class_str)
                 if cls is not None:
                     func = getattr(cls, func_str)
-                    setattr(cls, func_str, instrument(func, start_callback, end_callback, error_callback, isMethod=True))
+                    setattr(cls, func_str,
+                            instrument(func, source_string, start_callback, end_callback,
+                                       error_callback, isMethod=True))
                 else:
                     func = getattr(module, func_str)
                     setattr(module, func_str,
-                            instrument(func, start_callback, end_callback, error_callback))
-        except:
-            pass
+                            instrument(func, source_string, start_callback, end_callback,
+                                       error_callback))
+    except:
+        print("Error caught in hooking : {}".format(sys.exc_info()))
+        traceback.print_exc()
+    return module
